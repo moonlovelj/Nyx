9b2faa8b5af1ad2f63d6bac837cf2051b0802414
Moon <18500056398@163.com>
2025-08-01 11:42:22 +0800

    Add app and lib for deferred rendering

---
+++ MiniEngine/SceneViewer/Main.cpp
@@ -0,0 +1,63 @@
+#include "pch.h"
+#include "GameCore.h"
+#include "GraphicsCore.h"
+#include "SystemTime.h"
+#include "TextRenderer.h"
+#include "GameInput.h"
+#include "CommandContext.h"
+#include "RootSignature.h"
+#include "PipelineState.h"
+#include "BufferManager.h"
+
+using namespace GameCore;
+using namespace Graphics;
+
+class SceneViewer : public GameCore::IGameApp
+{
+public:
+
+    SceneViewer()
+    {
+    }
+
+    virtual void Startup( void ) override;
+    virtual void Cleanup( void ) override;
+
+    virtual void Update( float deltaT ) override;
+    virtual void RenderScene( void ) override;
+
+private:
+};
+
+CREATE_APPLICATION( SceneViewer )
+
+void SceneViewer::Startup( void )
+{
+    // Setup your data
+}
+
+void SceneViewer::Cleanup( void )
+{
+    // Free up resources in an orderly fashion
+}
+
+void SceneViewer::Update( float /*deltaT*/ )
+{
+    ScopedTimer _prof(L"Update State");
+
+    // Update something
+}
+
+void SceneViewer::RenderScene( void )
+{
+    GraphicsContext& gfxContext = GraphicsContext::Begin(L"Scene Render");
+
+    gfxContext.TransitionResource(g_SceneColorBuffer, D3D12_RESOURCE_STATE_RENDER_TARGET, true);
+    gfxContext.ClearColor(g_SceneColorBuffer);
+    gfxContext.SetRenderTarget(g_SceneColorBuffer.GetRTV());
+    gfxContext.SetViewportAndScissor(0, 0, g_SceneColorBuffer.GetWidth(), g_SceneColorBuffer.GetHeight());
+
+    // Rendering something
+
+    gfxContext.Finish();
+}
